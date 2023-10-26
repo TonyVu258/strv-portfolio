42f87428160c232df8511f40b35ff9fc91d55cbc
TonyVu258 <thienvu258.it@gmail.com>
2023-10-26 17:54:51 +0700

    update banner reel

--- src/components/Banner/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './banner.module.scss'
-
-function Banner() {
-  return (
-    <span>Banner</span>
-  )
-}
-
-export default Banner
---
+++ src/components/Section/Banner/index.js
@@ -0,0 +1,80 @@
+import styles from "./banner.module.scss";
+
+function BannerSection() {
+  return (
+    <span className={styles.bannerSection}>
+      <span className={styles.bannerWidth}>
+        <span style={{'display': 'inline-block', 'overflow': 'hidden'}}>
+          <span style={{'display':'inline-block'}}>
+            <span tabIndex='0' className={styles.bannerRed}>Next-Level</span>
+            <span className={styles.bannerResponsive}>
+              <span className={styles.bannerMobile}>
+                <span className={styles.bannerWhite}>
+                  Design
+                  <span className={styles.banner}>&</span>
+                  <span className={styles.bannerVideoSpan}>
+                    <figure className={styles.bannerFigure}>
+                      <div className={styles.bannerCover}>
+                        <button className={styles.bannerBtn}>
+                          <div className={styles.btnVideo}>
+                            <div className={styles.btnVideo_relative}>
+                              <video
+                                autoPlay
+                                playsInline
+                                preload="auto"
+                                muted
+                                loop
+                                className={styles.bannerVideo}
+                                src="https://stream.mux.com/8sr8qtxjwQKZ01qk00wzrGI7tDo8soLo35/low.mp4"
+                              />
+                              <div className={styles.bannerVideoItem} />
+                            </div>
+                          </div>
+                        </button>
+                      </div>
+                      <figcaption className={styles.bannerFigcaption}>
+                        STRV-REEL &rsquo;22
+                      </figcaption>
+                    </figure>
+                  </span>
+                </span>
+              </span>
+              <span className={styles.bannerDesktop}>
+                <span>
+                  <span className={styles.bannerVideoSpan}>
+                    <figure className={styles.bannerFigure}>
+                      <div className={styles.bannerCover}>
+                        <button className={styles.bannerBtn}>
+                          <div className={styles.btnVideo}>
+                            <div className={styles.btnVideo_relative}>
+                              <video
+                                autoPlay
+                                playsInline
+                                preload="auto"
+                                muted
+                                loop
+                                className={styles.bannerVideo}
+                                src="https://stream.mux.com/8sr8qtxjwQKZ01qk00wzrGI7tDo8soLo35/low.mp4"
+                              />
+                              <div className={styles.bannerVideoItem} />
+                            </div>
+                          </div>
+                        </button>
+                      </div>
+                      <figcaption className={styles.bannerFigcaption}>
+                        STRV-REEL &rsquo;22
+                      </figcaption>
+                    </figure>
+                  </span>
+                  Design<br/>& Engineering
+                </span>
+              </span>
+            </span>
+          </span>
+        </span>
+      </span>
+    </span>
+  );
+}
+
+export default BannerSection;
