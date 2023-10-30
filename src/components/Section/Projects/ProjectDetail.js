c4ba73691486386c3f720d613dd88a4a17314f84
TonyVu <thienvu258.it@gmail.com>
2023-10-30 11:44:55 +0700

    Add files via upload
    update project section

--- src/components/Section/Projects/ProjectDetail.js
+++ src/components/Section/Projects/ProjectDetail.js
@@ -1,29 +1,38 @@
+import Link from "next/link";
 import styles from "./project.module.scss";
 
-function ProjectDetail({index, dataImage1, dataImage2, title, subtitle, projectLink}) {
+function ProjectDetail({
+  index,
+  dataImage1,
+  dataImage2,
+  dataImage3,
+  imgAlt,
+  title,
+  subtitle,
+  projectLink,
+}) {
   return (
     <div className={`styles.project_${index}`}>
-      <figure className={`styles.project_${index}_box`}>
-        <Link href={projectLink? projectLink: '/'} className={`styles.project_${index}_link`}>
-          <div className={`styles.project_${index}_img`}>
-            <img
-              src={dataImage1}
-              aria-hidden="true"
-              alt=""
-            />
-            <img
-              aria-hidden="true"
-              alt=""
-              src={dataImage2}
-            />
+      <figure>
+        <Link
+          href={projectLink ? projectLink : "/"}
+          className={`styles.project_${index}_link`}
+        >
+          <div className={`styles.project_img`}>
+            <div className={`styles.project_img_data`}>
+              <img src={dataImage1} aria-hidden="true" alt="" className={`styles.project_img_data1`}/>
+              <img aria-hidden="true" alt="" src={dataImage2} className={`styles.project_img_data2`}/>
+              <img src={dataImage3} alt={imgAlt} className={`styles.project_img_data3`}/>
+            </div>
           </div>
         </Link>
         <figcaption className={`styles.project_${index}_caption`}>
-          <Link href={projectLink? projectLink: '/'} className={`styles.project_${index}_detail`}>
+          <Link
+            href={projectLink ? projectLink : "/"}
+            className={`styles.project_${index}_detail`}
+          >
             <h3 className={`styles.project_${index}_title`}>{title}</h3>
-            <span className={`styles.project_${index}_note`}>
-              {subtitle}
-            </span>
+            <span className={`styles.project_${index}_note`}>{subtitle}</span>
           </Link>
         </figcaption>
       </figure>
--- src/components/Section/Projects/index.js
+++ src/components/Section/Projects/index.js
@@ -1,17 +1,46 @@
 import Link from "next/link";
 import styles from "./project.module.scss";
+import ProjectDetail from "./ProjectDetail";
 
 function ProjectSection() {
   return (
     <div className={styles.projectSection}>
       <div className={styles.projectBox}>
         <h2 className={styles.projectBoxContent}>
-        <svg fill="none" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" className={styles.projectBoxSVG}><path fill-rule="evenodd" clip-rule="evenodd" d="M11.2396 6.99425 6.58589 2.34052 7.99896.92517l7.06694 7.06695-.0005.00056.0044.00446-7.06126 7.07266-1.41421-1.4142 4.65307-4.66055-9.02388.00363-.0445.00002-.00081-2.00081.0445-.00002.00041 1.00041-.0004-1.00041 9.01688-.00362Z" fill="currentColor"></path></svg>
-        Selected projects
+          <svg
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            className={styles.projectBoxSVG}
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M11.2396 6.99425 6.58589 2.34052 7.99896.92517l7.06694 7.06695-.0005.00056.0044.00446-7.06126 7.07266-1.41421-1.4142 4.65307-4.66055-9.02388.00363-.0445.00002-.00081-2.00081.0445-.00002.00041 1.00041-.0004-1.00041 9.01688-.00362Z"
+              fill="currentColor"
+            ></path>
+          </svg>
+          Selected projects
         </h2>
       </div>
-      
-      <div className={styles.project_three}></div>
+      <ProjectDetail
+        index={1}
+        dataImage1={
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNzIwIiBoZWlnaHQ9IjE3MjAiPjwvc3ZnPg=="
+        }
+        dataImage2={
+          "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHBwgHBgoQEggSChUWGhcNDQ0NDhUNDh0OFxowGBYfFhUmHysjHSkoHRUWJDUlKC0vMjIyGSI4PTcwPCsxMi8BCgsLDg0OHBAQHDsmIiU7OzsvNTs7NTIvLy82Ly8vLy8vLzU8NS8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vL//AABEIABgAGAMBIgACEQEDEQH/xAAZAAEAAwEBAAAAAAAAAAAAAAAABAUGBwL/xAAdEAACAgMBAQEAAAAAAAAAAAAAAgMFAQQxIRIT/8QAGAEAAgMAAAAAAAAAAAAAAAAABQYAAwT/xAAaEQEAAgMBAAAAAAAAAAAAAAABAAIDETIi/9oADAMBAAIRAxEAPwDxJp/onCDuV/zDnw1SpD8ELfWPEefCNnUsxW9zm1hrZibgLW/zGicBgsqxnx2GpNq8+I1KG5vEgUALNTUU8XcydlaptpwAA6wbjDW6AE//2Q=="
+        }
+        dataImage3={
+          "https://www.datocms-assets.com/57452/1639686148-barrys-1x1.jpg"
+        }
+        imgAlt={"Barry's X instructor posing"}
+        title={"Barry's"}
+        subtitle={"PM, DESIGN, iOS, ANDROID, WEB"}
+        projectLink={""}
+      ></ProjectDetail>
       <div className={styles.project_four}></div>
       <div className={styles.project_five}></div>
       <div className={styles.project_six}></div>
