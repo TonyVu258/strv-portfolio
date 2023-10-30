c382999516ea9db9ac4edaa1e2ddc7b0647bf70a
TonyVu <thienvu258.it@gmail.com>
2023-10-30 18:00:19 +0700

    update project section

--- src/components/Section/About/index.js
+++ src/components/Section/About/index.js
@@ -16,19 +16,19 @@ function AboutSection() {
                 playsInline
                 preload="auto"
                 loop
-                
+
                 src="https://www.datocms-assets.com/57452/1639681351-home-intro-looper.mp4"
               />
-              <div className={styles.aboutVideo}/>
+              <div className={styles.aboutVideo} />
             </div>
           </div>
         </div>
-        &nbsp;Our vision has always been simple: 
+        &nbsp;Our vision has always been simple:
         <span className={styles.aboutItem_red}>&nbsp;We want to do top work.</span>
         &nbsp;And today, with a team of 190 experts that continue striving for more,
-        we&rsquo;re still as ready as ever to go the 
+        we&rsquo;re still as ready as ever to go the
         <Link href="/" className={styles.aboutItem_underline}>
-        &nbsp;extra mile
+          &nbsp;extra mile
           <span className={styles.aboutItem_sup}>&nbsp;(view services)</span>
         </Link>
         &nbsp;for our craft,&nbsp;
@@ -40,16 +40,16 @@ function AboutSection() {
       </p>
       <div className={styles.aboutBtns}>
         <Link href='/' className={styles.aboutContactBtn}>
-            <div className={styles.aboutBtnContent}>
-                Let&rsquo;s talk
+          <div className={styles.aboutBtnContent}>
+            Let&rsquo;s talk
             <svg fill="none" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" className={styles.aboutBtnSVG}><path fill-rule="evenodd" clip-rule="evenodd" d="M11.2396 6.99425 6.58589 2.34052 7.99896.92517l7.06694 7.06695-.0005.00056.0044.00446-7.06126 7.07266-1.41421-1.4142 4.65307-4.66055-9.02388.00363-.0445.00002-.00081-2.00081.0445-.00002.00041 1.00041-.0004-1.00041 9.01688-.00362Z" fill="currentColor"></path></svg>
-            </div>
+          </div>
         </Link>
         <Link href='/' className={styles.aboutWhyUsBtn}>
-            <div className={styles.aboutBtnContent}>
-                Why STRV?
+          <div className={styles.aboutBtnContent}>
+            Why STRV?
             <svg fill="none" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" className={styles.aboutBtnSVG}><path fill-rule="evenodd" clip-rule="evenodd" d="M11.2396 6.99425 6.58589 2.34052 7.99896.92517l7.06694 7.06695-.0005.00056.0044.00446-7.06126 7.07266-1.41421-1.4142 4.65307-4.66055-9.02388.00363-.0445.00002-.00081-2.00081.0445-.00002.00041 1.00041-.0004-1.00041 9.01688-.00362Z" fill="currentColor"></path></svg>
-            </div>
+          </div>
         </Link>
       </div>
     </div>
--- src/components/Section/Projects/ProjectDetail.js
+++ src/components/Section/Projects/ProjectDetail.js
@@ -12,28 +12,22 @@ function ProjectDetail({
   projectLink,
 }) {
   return (
-    <div className={`styles.project_${index}`}>
+    <div className={styles[`project_${index}`]}> {/* Use dynamic class name */}
       <figure>
-        <Link
-          href={projectLink ? projectLink : "/"}
-          className={`styles.project_${index}_link`}
-        >
-          <div className={`styles.project_img`}>
-            <div className={`styles.project_img_data`}>
-              <img src={dataImage1} aria-hidden="true" alt="" className={`styles.project_img_data1`}/>
-              <img aria-hidden="true" alt="" src={dataImage2} className={`styles.project_img_data2`}/>
-              <img src={dataImage3} alt={imgAlt} className={`styles.project_img_data3`}/>
+        <Link href={projectLink ? projectLink : "/"} className={styles.project_link}>
+          <div className={styles.project_img}>
+            <div className={styles[`project_img_${index}_data`]}>
+              <img src={dataImage1} aria-hidden="true" alt="" className={styles.project_img_data1}/>
+              <img aria-hidden="true" alt="" src={dataImage2} className={styles.project_img_data2}/>
+              <img src={dataImage3} alt={imgAlt} className={styles.project_img_data3}/>
             </div>
           </div>
         </Link>
-        <figcaption className={`styles.project_${index}_caption`}>
-          <Link
-            href={projectLink ? projectLink : "/"}
-            className={`styles.project_${index}_detail`}
-          >
-            <h3 className={`styles.project_${index}_title`}>{title}</h3>
-            <span className={`styles.project_${index}_note`}>{subtitle}</span>
+        <figcaption className={styles.project_caption}>
+          <Link href={projectLink ? projectLink : "/"} className={styles.project_detail}>
+            <h3 className={styles.project_title}>{title}</h3>
           </Link>
+            {subtitle && <span className={styles.project_note}>{subtitle}</span>}
         </figcaption>
       </figure>
     </div>
